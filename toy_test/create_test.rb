354c649e3fd261ea5bcc13ea98ee957a328eb340
Gaf <gaf@gaf-OptiPlex-760.(none)>
2013-08-23 10:52:15 +0200

    +Add toy test

---
+++ toy_test/create_test.rb
@@ -0,0 +1,29 @@
+#!/usr/bin/env ruby
+
+require 'mongo'
+include Mongo
+
+if __FILE__ == $0
+	mongo_client = MongoClient.new
+	db = mongo_client.db("device_tracking_from_prod")
+	coll = db.collection("session")
+	cookies = coll.aggregate([
+		{ "$group" => {_id: "$user_cookie",n: {"$sum" => 1}}},
+		{ "$sort" => {n: -1}},
+		{ "$limit" => 10}
+		])
+	cntr = 0
+	File.open("./top_cookies.txt", 'w') do |file|
+		cookies.each do |e|
+			cursor = coll.find("user_cookie" => e["_id"])
+			cursor.each do |row|
+				next if row["browser"].nil?
+				plugin_string = row["browser"]["plugins"].collect {|plugin| plugin.values.join}
+				plugin_string = plugin_string.join().gsub(/"/, "")
+				file.puts "#{cntr}|#{plugin_string}"
+				cntr += 1
+			end
+		end
+	end
+
+end
