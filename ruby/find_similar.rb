8118a059bbefbf4681aabe51638056efbefe8c9c
Kitton <alval.bel@gmail.com>
2013-09-10 12:58:11 +0200

    *Move find similar into a function

--- ruby/find_similar.rb
+++ ruby/find_similar.rb
@@ -1,9 +1,6 @@
 #!/usr/bin/env ruby
 # encoding: utf-8
 
-raise "usage: find_similar.rb SIMILARITY_PERCENTAGE=[0..1]" unless ARGV.length == 1
-SIMILARITY_PERCENTAGE = ARGV[0].to_f
-
 require 'set'
 require 'mongo'
 include Mongo
@@ -19,51 +16,55 @@ def puts msg
 	_puts "#{Time.now} #{msg}"
 end
 
-db = MongoClient.new.db("Sketching")
 
-coll = db["Parameters"]
-parameters = coll.find_one()
-p "parameters = #{parameters}"
-SKETCH_SIZE = parameters["SKETCH_SIZE"]
-N_GRAM_LEN = parameters["N_GRAM_LEN"]
-SHINGLE_MODE = parameters["SHINGLE_MODE"]
+def find_similar searched_text, similarity_percentage
+	db = MongoClient.new.db("Sketching")
 
-documents = read_data
+	coll = db["Parameters"]
+	parameters = coll.find_one()
+	p "parameters = #{parameters}"
+	sketch_size = parameters["SKETCH_SIZE"]
+	n_gram_len = parameters["N_GRAM_LEN"]
+	shingle_mode = parameters["SHINGLE_MODE"]
 
-searched_id = -1
-document_shingles = documents.collect do |id,text|
-	searched_id = id
-	text.shingles
+	document_shingles = searched_text.shingles(shingle_mode, n_gram_len)
+
+	sketches = Sketches.calculate_for([document_shingles], sketch_size)
+	# sketches = Sketches.calculate_for_all document_shingles
+	sketches = sketches.collect{|s| s.sketch}
+	sketches.flatten!
+	p "sketches.size = #{sketches.size}"
+
+	coll = db["Sketches"]
+
+	similar_docs = {}
+	similar_docs.default = 0
+
+	sketches.each do |s|
+		doc = coll.find_one("sketch" => s.to_s)
+		next if doc.nil?
+		doc["doc_ids"].each {|e| similar_docs[e] += 1}
+	end
+
+	# p similar_docs.sort_by {|_key, value| value}
+	near_duplicates = []
+	max_similarity = similar_docs.values.max
+	similar_docs.each {|k,v| near_duplicates << k if v >= max_similarity*similarity_percentage}
+	p "max_similarity = #{max_similarity} from #{sketch_size}"
+
+	coll = db["Documents"]
+	near_duplicates.each do |id|
+		doc = coll.find_one("_id" => id)
+		next if doc.nil?
+		p doc
+	end
+
+	# calc_dist(searched_id, near_duplicates)
 end
 
-sketches = Sketches.calculate_for document_shingles
-# sketches = Sketches.calculate_for_all document_shingles
-sketches = sketches.collect{|s| s.sketch}
-sketches.flatten!
-p "sketches.size = #{sketches.size}"
-
-coll = db["Sketches"]
-
-similar_docs = {}
-similar_docs.default = 0
-
-sketches.each do |s|
-	doc = coll.find_one("sketch" => s.to_s)
-	next if doc.nil?
-	doc["doc_ids"].each {|e| similar_docs[e] += 1}
+if __FILE__ == $0
+	raise "usage: find_similar.rb SEARCHED_TEXT SIMILARITY_PERCENTAGE=[0..1]" unless ARGV.length == 2
+	searched_text = ARGV[0].dup
+	similarity_percentage = ARGV[1].to_f
+	find_similar(searched_text, similarity_percentage)
 end
-
-# p similar_docs.sort_by {|_key, value| value}
-near_duplicates = []
-MAX_SIMILARITY = similar_docs.values.max
-similar_docs.each {|k,v| near_duplicates << k if v >= MAX_SIMILARITY*SIMILARITY_PERCENTAGE}
-p "MAX_SIMILARITY = #{MAX_SIMILARITY} from #{SKETCH_SIZE}"
-
-coll = db["Documents"]
-near_duplicates.each do |id|
-	doc = coll.find_one("_id" => id)
-	next if doc.nil?
-	p doc
-end
-
-# calc_dist(searched_id, near_duplicates)
