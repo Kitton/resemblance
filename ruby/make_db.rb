89f0b068b0c36486ee60d911fb1640fa07bd6fe1
Kitton <alval.bel@gmail.com>
2013-09-10 12:13:19 +0200

    +Reduce memory consumption during make db

--- ruby/make_db.rb
+++ ruby/make_db.rb
@@ -19,51 +19,92 @@ def puts msg
 	_puts "#{Time.now} #{msg}"
 end
 
-documents = read_data
-
-puts "documents are read"
-
-document_ids = [] # mapping from idx in arrays to document id read from data file
-document_shingles = documents.collect do |id_text|
-	id,text = id_text
-	document_ids << id
-	text.shingles
-end
-
-puts "Shingles are created"
-
-sketches = Sketches.calculate_for document_shingles
-puts "Sketches are calculated"
-
-# build < xid, di > list and convert into hash xi -> ids_of_those_with_xi
-x_to_i = {}
-sketches.each_with_index do |s,i|
-	p s.inspect if s.sketch.nil?
-	s.sketch.each do |sk|
-		x_to_i[sk] ||= Set.new
-		x_to_i[sk] << document_ids[i]
-	end
-end
-
 MongoClient.new.drop_database("Sketching")
 db = MongoClient.new.db("Sketching")
-
-#Save Parameters
 coll = db["Parameters"]
 doc = {"SKETCH_SIZE" => SKETCH_SIZE, "N_GRAM_LEN" =>N_GRAM_LEN, "SHINGLE_MODE" => SHINGLE_MODE}
 coll.insert(doc)
 
-coll = db["Documents"]
+documents = read_data
+puts "documents are read"
 
+coll = db["Documents"]
 documents.each do |id, text|
 	doc = {"_id" => id, "Doc_text" => text}
 	coll.insert(doc)
 end
+puts "Docs are inserted"
 
-puts "Documents are inserted"
-
+# sketches_with_ids = {}
 coll = db["Sketches"]
-x_to_i.each do |sketch, doc_ids|
-	doc = {"sketch" => sketch.to_s, "doc_ids" => doc_ids.to_a}
-	coll.insert(doc)
+coll.create_index("sketch")
+
+documents.each do |id, text|
+	puts id if ( (id % 20000) == 0)
+	doc_shingles = text.shingles
+	sketches = Sketch.new(doc_shingles).apply_hash 
+	sketches.each do |sk|
+		doc = coll.find_one("sketch" => sk.to_s)
+		if doc.nil?
+			new_doc = {"sketch" => sk.to_s, "doc_ids" => [id]}
+			coll.insert(new_doc)
+		else
+			coll.update({"sketch" => sk.to_s}, {"$addToSet" => {"doc_ids" => id}})
+		end
+		# sketches_with_ids[sk] ||= Set.new
+		# sketches_with_ids[sk] << id
+	end
 end
+
+# sketches_with_ids.each do |sk, ids|
+# 	doc = {"sketch" => sk.to_s, "doc_ids" => ids.to_a}
+# 	coll.insert(doc)
+# end
+
+
+# document_ids = [] # mapping from idx in arrays to document id read from data file
+# document_shingles = documents.collect do |id_text|
+# 	id,text = id_text
+# 	document_ids << id
+# 	text.shingles
+# end
+
+# puts "Shingles are created"
+
+# sketches = Sketches.calculate_for document_shingles
+# puts "Sketches are calculated"
+
+# # build < xid, di > list and convert into hash xi -> ids_of_those_with_xi
+# x_to_i = {}
+# sketches.each_with_index do |s,i|
+# 	p s.inspect if s.sketch.nil?
+# 	s.sketch.each do |sk|
+# 		x_to_i[sk] ||= Set.new
+# 		x_to_i[sk] << document_ids[i]
+# 	end
+# end
+
+# MongoClient.new.drop_database("Sketching")
+# db = MongoClient.new.db("Sketching")
+
+# #Save Parameters
+# coll = db["Parameters"]
+# doc = {"SKETCH_SIZE" => SKETCH_SIZE, "N_GRAM_LEN" =>N_GRAM_LEN, "SHINGLE_MODE" => SHINGLE_MODE}
+# coll.insert(doc)
+
+# coll = db["Documents"]
+
+# documents.each do |id, text|
+# 	doc = {"_id" => id, "Doc_text" => text}
+# 	coll.insert(doc)
+# end
+
+# puts "Documents are inserted"
+
+# coll = db["Sketches"]
+# x_to_i.each do |sketch, doc_ids|
+# 	doc = {"sketch" => sketch.to_s, "doc_ids" => doc_ids.to_a}
+# 	coll.insert(doc)
+# end
+
+puts "<--- end time"
