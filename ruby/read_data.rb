8132113095a57122dff075c1af9039ea9330ea29
Kitton <alval.bel@gmail.com>
2013-09-16 13:19:40 +0200

    +Read more data from INE base

--- ruby/read_data.rb
+++ ruby/read_data.rb
@@ -7,10 +7,14 @@
 def read_data stream=STDIN
 	data = {}
 	stream.each do |line|
-		line =~ /^([0-9]*)\|(.*)/;
-		id, name_addr = $1.to_i, $2
+		# line =~ /^([0-9]*)\|(.*)/;
+		# id, name_addr = $1.to_i, $2
+		splitted = line.split("|")
+		id, addr, street_name, city, zipcode, censal_code = splitted
+		id = id.to_i
+
 		raise "duplicate id #{id}?" if data[id]
-		data[id] = name_addr
+		data[id] = {"addr" => addr, "street_name" => street_name, "city" => city, "zipcode" => zipcode, "censal_code" => censal_code.strip}
 	end
 	data
 end
